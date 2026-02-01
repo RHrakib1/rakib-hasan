8bacce760e0a3d76b236793e753beef1f7d2f8e7
rakib hasan <rakibhasanmd457@gmail.com>
2026-02-01 19:55:24 +0600

    add next part

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ function sum(number1, number2) {
     const mainValue = number1 + number2;
     console.log("The Data is a :", number1, number2, mainValue)
 }
-sum(10, 50);
+// sum(10, 50);
 
 function multipal(num1, num2, num3) {
     mostnumber = num1 * num2 * num3;
     console.log(mostnumber);
 }
-multipal(2, 8, 6)
+// multipal(2, 8, 6)
+
+const value21 = (ab, bc) => (ab + bc)
+
+const hbe = value21(10, 20);
+console.log(hbe)
