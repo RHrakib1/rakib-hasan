00a5044ca1ad83ef9a3183af7a79267fdaaf7cfe
rakib hasan <rakibhasanmd457@gmail.com>
2026-02-01 19:25:12 +0600

    add next part

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-function sum (number1,number2){
+function sum(number1, number2) {
     const mainValue = number1 + number2;
-    console.log("The Data is a :",number1,number2,mainValue) 
+    console.log("The Data is a :", number1, number2, mainValue)
 }
-sum(10,50);
+sum(10, 50);
+
+function multipal(num1, num2, num3) {
+    mostnumber = num1 * num2 * num3;
+    console.log(mostnumber);
+}
+multipal(2, 8, 6)
