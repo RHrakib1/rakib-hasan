0ff431305b38e4c18ea1f16d36e1ab967e241624
rakib hasan <rakibhasanmd457@gmail.com>
2026-02-23 12:09:27 +0600

    add new oparator

--- index.js
+++ index.js
@@ -21,7 +21,30 @@ const names = ['rakib', 'hasan', 'tarin', 'torabi', 'rahman'];
 console.log(...names);
 console.log(names);
 
-// const baynumber = (2, 10, 3, 85, 111, 5520, 787840, 1000122, 50)
+const baynumber = [2555555, 10, 3, 85, 111, 5520, 787840, 1000122, 50]
 
-const bytotal = Math.max(10,222,405,1555,11,220,20);
+const bytotal = Math.max(...baynumber);
 console.log(bytotal);
+
+
+const first = [1, 2, 4, 5, 7]
+const second = first;
+second.push(10);
+console.log('the first number is a :', first);
+console.log('the second number is a :', second);
+
+
+const pothom = [1, 2, 3, 4, 5, 6, 7];
+const ses = [...pothom];
+ses.push(100);
+console.log('ses number aita :', ses);
+console.log('pothom number aita :', pothom);
+
+const rakibfun = (a, c, v, z) => a + c + v + z;
+const digit = [10, 5, 78, 62];
+console.log(rakibfun(...digit))
+
+const sonkha = [45, 89, 24, 38, 16, 10]
+
+const totalValue = sonkha.map(number => number - 2)
+console.log('map value is a :', totalValue);
