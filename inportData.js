89eb213814281a17247ad648dea7ac6fbca7aa84
rakib hasan <rakibhasanmd457@gmail.com>
2026-02-28 16:13:56 +0600

    again practices

--- inportData.js
+++ inportData.js
@@ -2,5 +2,12 @@ const dataloadwebsite = () => {
     const url = ('https://jsonplaceholder.typicode.com/users');
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => display(data))
+}
+const display = (posts) => {
+    const poestcontainer = document.getElementById('post-container')
+    poestcontainer.innerHTML('')
+    posts.forEach((post) => {
+        console.log(post);
+    })
 }
