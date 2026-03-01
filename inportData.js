efdaef9801c4283327f5a9f6c101a16fb4f981d1
rakib hasan <rakibhasanmd457@gmail.com>
2026-03-01 16:17:05 +0600

    new card

--- inportData.js
+++ inportData.js
@@ -1,13 +1,31 @@
 const dataloadwebsite = () => {
-    const url = ('https://jsonplaceholder.typicode.com/users');
+    const url = 'https://jsonplaceholder.typicode.com/users';
     fetch(url)
         .then(res => res.json())
         .then(data => display(data))
 }
-const display = (posts) => {
-    const poestcontainer = document.getElementById('post-container')
-    poestcontainer.innerHTML('')
-    posts.forEach((post) => {
-        console.log(post);
-    })
+
+const display = (users) => {
+    const postcontainer = document.getElementById('post-container');
+    postcontainer.innerHTML = '';
+
+    users.forEach((user) => {
+        const postcard = document.createElement("div");
+
+        postcard.innerHTML = `
+        <div class="post-card">
+            <h2>${user.name}</h2>
+            <div class="username">@${user.username}</div>
+
+            <div class="info">📧 ${user.email}</div>
+            <div class="info">📞 ${user.phone}</div>
+            <div class="info">🌐 ${user.website}</div>
+            <div class="info">📍 ${user.address.city}</div>
+
+            <div class="company">🏢 ${user.company.name}</div>
+        </div>
+        `;
+
+        postcontainer.append(postcard);
+    });
 }
